7678992ce2a04d77f7d591da98fb13a5d5139a6b
jiminKim <programmer0415@gmail.com>
2022-04-07 12:31:03 +0900

    Feat: 서블릿으로 회원관리 기능 추가

---
+++ src/main/java/hello/servlet/web/servelet/MemberListServlet.java
@@ -0,0 +1,58 @@
+package hello.servlet.web.servelet;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+//자바 코드에 html을 넣는다...
+//JSP, tymeLeaf 를 통해서 템플릿 엔진을 사용하면 사용이 가능하다. JSP 를 다루고 그다음엔 tymeleaf를 사용한다.
+@WebServlet(name= "memberListServlet", urlPatterns = "/servlet/members")
+public class MemberListServlet extends HttpServlet {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+         List<Member> members= memberRepository.findAll();
+
+         resp.setContentType("text/html");
+         resp.setCharacterEncoding("utf-8");
+
+        PrintWriter w = resp.getWriter();
+        w.write("<html>");
+        w.write("<head>");
+        w.write("    <meta charset=\"UTF-8\">");
+        w.write("    <title>Title</title>");
+        w.write("</head>");
+        w.write("<body>");
+        w.write("<a href=\"/index.html\">메인</a>");
+        w.write("<table>");
+        w.write("    <thead>");
+        w.write("    <th>id</th>");
+        w.write("    <th>username</th>");
+        w.write("    <th>age</th>");
+        w.write("    </thead>");
+        w.write("    <tbody>");
+
+        for (Member member : members) {
+            w.write("    <tr>");
+            w.write("       <td>" + member.getId() + "</td>");
+            w.write("       <td>" + member.getUsername() + "</td>");
+            w.write("       <td>" + member.getAge() + "</td>");
+            w.write("    </tr>");
+        }
+        w.write("    </tbody>");
+        w.write("</table>");
+        w.write("</body>");
+        w.write("</html>");
+
+    }
+}
